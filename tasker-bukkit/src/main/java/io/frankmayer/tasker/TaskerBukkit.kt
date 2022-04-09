e83be29c2a7cfc74f9a37c256bd5d8a44ebefc74
Frank <mail@frank-mayer.io>
2022-04-09 11:39:30 +0200

    kotlin

---
+++ tasker-bukkit/src/main/java/io/frankmayer/tasker/TaskerBukkit.kt
@@ -0,0 +1,13 @@
+package io.frankmayer.tasker
+
+import org.bukkit.plugin.java.JavaPlugin
+
+class TaskerBukkit : JavaPlugin() {
+    override fun onEnable() {
+        // Plugin startup logic
+    }
+
+    override fun onDisable() {
+        // Plugin shutdown logic
+    }
+}
---
+++ tasker-bungeecord/src/main/java/io/frankmayer/tasker/TaskerBungeeCord.kt
@@ -0,0 +1,13 @@
+package io.frankmayer.tasker
+
+import net.md_5.bungee.api.plugin.Plugin
+
+class TaskerBungeeCord : Plugin() {
+    override fun onEnable() {
+        // Plugin startup logic
+    }
+
+    override fun onDisable() {
+        // Plugin shutdown logic
+    }
+}
